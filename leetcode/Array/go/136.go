8cd98d48a115b02e8279fda06a5fa3655b36c03c
Fatih <fatihsen539@gmail.com>
2022-03-24 21:56:23 -1000

    Create 136.go

---
+++ leetcode/Array/go/136.go
@@ -0,0 +1,12 @@
+func singleNumber(nums []int) int {
+    nap := make(map[int]int)
+    for _, v := range nums {
+        nap[v]++
+    }
+    for k, x := range nap {
+        if x == 1 {
+            return k
+        }
+    }
+    return -1
+}
